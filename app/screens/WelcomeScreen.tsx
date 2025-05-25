7ad9d9a9171af3f8e118abc9e63c024a3c59e63f
Your Name <you@example.com>
2025-05-25 05:24:59 +0100

    nonunion 1 changes

--- app/screens/WelcomeScreen.tsx
+++ app/screens/WelcomeScreen.tsx
@@ -1,83 +1,142 @@
 import { observer } from "mobx-react-lite"
 import { FC } from "react"
-import { Image, ImageStyle, TextStyle, View, ViewStyle } from "react-native"
+import { Image, StyleSheet, TextInput, TouchableOpacity, View } from "react-native"
 import { Text, Screen } from "@/components"
-import { isRTL } from "@/i18n"
+import { useState } from "react"
+
+import { Picker } from "@react-native-picker/picker"
+
 import { AppStackScreenProps } from "../navigators"
-import { $styles, type ThemedStyle } from "@/theme"
-import { useSafeAreaInsetsStyle } from "../utils/useSafeAreaInsetsStyle"
+import { $styles } from "@/theme"
+// import { useSafeAreaInsetsStyle } from "../utils/useSafeAreaInsetsStyle"
 import { useAppTheme } from "@/utils/useAppTheme"
-
-const welcomeLogo = require("../../assets/images/logo.png")
-const welcomeFace = require("../../assets/images/welcome-face.png")
-
 interface WelcomeScreenProps extends AppStackScreenProps<"Welcome"> {}
 
 export const WelcomeScreen: FC<WelcomeScreenProps> = observer(function WelcomeScreen() {
-  const { themed, theme } = useAppTheme()
+  const { theme } = useAppTheme()
+  const handleAddEntry = () => {
+    console.log("Add Entry button pressed")
+  }
 
-  const $bottomContainerInsets = useSafeAreaInsetsStyle(["bottom"])
+  const [selectedProject, setSelectedProject] = useState("Project A")
+  const [notes, setNotes] = useState("")
+  const [startTime, setStartTime] = useState("")
+  const [endTime, setEndTime] = useState("")
 
   return (
     <Screen preset="fixed" contentContainerStyle={$styles.flex1}>
-      <View style={themed($topContainer)}>
-        <Image style={themed($welcomeLogo)} source={welcomeLogo} resizeMode="contain" />
-        <Text
-          testID="welcome-heading"
-          style={themed($welcomeHeading)}
-          tx="welcomeScreen:readyForLaunch"
-          preset="heading"
-        />
-        <Text tx="welcomeScreen:exciting" preset="subheading" />
+      <View style={styles.container}>
         <Image
-          style={$welcomeFace}
-          source={welcomeFace}
           resizeMode="contain"
           tintColor={theme.isDark ? theme.colors.palette.neutral900 : undefined}
         />
-      </View>
+        <Text style={styles.header}>📅 Add Timesheet</Text>
+        <Text style={styles.label}>Sheet Date</Text>
+        <TextInput style={styles.readOnlyInput} value="23 JAN 2023" editable={true} />
+        <Text style={styles.label}>Project</Text>
+        <Picker
+          selectedValue={selectedProject}
+          onValueChange={(itemValue) => setSelectedProject(itemValue)}
+          style={styles.readOnlyInput}
+        >
+          <Picker.Item label="Training " value="Training" />
+          <Picker.Item label="Edison Tower Power " value="Edison Tower Power" />
+          <Picker.Item label="IS 45X " value="IS 45X" />
+        </Picker>
+        <Text style={styles.label}>Notes</Text>
+        <TextInput
+          placeholder="Enter notes here"
+          value={notes}
+          onChangeText={setNotes}
+          style={styles.input}
+          multiline
+          numberOfLines={4}
+        />
+        <Text style={styles.label}>Start Time</Text>
+        <TextInput
+          placeholder="7.00 AM"
+          value={startTime}
+          onChangeText={setStartTime}
+          style={styles.input}
+        />
 
-      <View style={themed([$bottomContainer, $bottomContainerInsets])}>
-        <Text tx="welcomeScreen:postscript" size="md" />
+        <Text style={styles.label}>End Time</Text>
+        <TextInput
+          placeholder="3.00 PM"
+          value={endTime}
+          onChangeText={setEndTime}
+          style={styles.input}
+        />
+
+        <View style={styles.centeredButtonContainer}>
+          <TouchableOpacity style={styles.button} onPress={handleAddEntry}>
+            <Text style={styles.buttonText}> + Add Entry</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </Screen>
   )
 })
+const styles = StyleSheet.create({
+  button: {
+    // backgroundColor: theme.colors.background,
+    borderRadius: 8,
+    paddingHorizontal: 24,
+    paddingVertical: 12,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  centeredButtonContainer: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+  },
+  container: {
+    flex: 1,
+    padding: 30,
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginBottom: 20,
+  },
 
-const $topContainer: ThemedStyle<ViewStyle> = ({ spacing }) => ({
-  flexShrink: 1,
-  flexGrow: 1,
-  flexBasis: "57%",
-  justifyContent: "center",
-  paddingHorizontal: spacing.lg,
+  input: {
+    borderRadius: 5,
+    borderWidth: 1,
+    marginBottom: 15,
+    padding: 10,
+  },
+  label: {
+    fontSize: 14,
+    marginBottom: 5,
+  },
+
+  readOnlyInput: {
+    borderRadius: 6,
+    marginBottom: 20,
+    padding: 10,
+  },
 })
 
-const $bottomContainer: ThemedStyle<ViewStyle> = ({ colors, spacing }) => ({
-  flexShrink: 1,
-  flexGrow: 0,
-  flexBasis: "43%",
-  backgroundColor: colors.palette.neutral100,
-  borderTopLeftRadius: 16,
-  borderTopRightRadius: 16,
-  paddingHorizontal: spacing.lg,
-  justifyContent: "space-around",
-})
+// const $topContainer: ThemedStyle<ViewStyle> = ({ spacing }) => ({
+//   flexShrink: 1,
+//   flexGrow: 1,
+//   flexBasis: "57%",
+//   justifyContent: "center",
+//   paddingHorizontal: spacing.lg,
+// })
+// const $title: TextStyle = {
+//   fontSize: 18,
+//   fontWeight: "bold",
+//   marginBottom: 360,
+// }
 
-const $welcomeLogo: ThemedStyle<ImageStyle> = ({ spacing }) => ({
-  height: 88,
-  width: "100%",
-  marginBottom: spacing.xxl,
-})
-
-const $welcomeFace: ImageStyle = {
-  height: 169,
-  width: 269,
-  position: "absolute",
-  bottom: -47,
-  right: -80,
-  transform: [{ scaleX: isRTL ? -1 : 1 }],
-}
-
-const $welcomeHeading: ThemedStyle<TextStyle> = ({ spacing }) => ({
-  marginBottom: spacing.md,
-})
+// const $buttonContainer: ViewStyle = {
+//   backgroundColor: "#007AFF",
+//   paddingVertical: 12,
+//   paddingHorizontal: 32,
+//   borderRadius: 8,
+// }
